547f2dc958e4db13db42af01d055fa198ac69efd
gsrkuang <gsrkuang@163.com>
2022-02-25 11:43:48 +0800

    删除不用的api

--- app/src/main/java/com/colin/util/Constants.java
+++ app/src/main/java/com/colin/util/Constants.java
@@ -3,11 +3,6 @@ package com.colin.util;
 public class Constants {
 
     /**
-     * 阳光海滩首页
-     */
-    public static final String SUNNY_BEACH_SITE_BASE_URL = "https://www.sunofbeach.net/";
-
-    /**
      * 阳光海滩 api 接口前缀
      */
     public static final String SUNNY_BEACH_API_BASE_URL = "https://api.sunofbeaches.com/";
